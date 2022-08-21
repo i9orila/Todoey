bad6a293ca80e6bbb6dcb2c89e22e68c9b814629
I9orila <i9orila@aol.com>
2022-08-21 16:52:02 +0300

    functional Add items of tableView, add Canvas of CwiftUI

---
+++ Todoey/Controller/ToDoListViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ToDoListViewController.swift
+//  Todoey
+//
+//  Created by I9orila T on 15.08.2022.
+//
+
+import UIKit
+import SwiftUI
+
+class ToDoListViewController: UITableViewController {
+    
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK - Tableview Datasorse Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    //MARK - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title:"Add new Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            self.itemArray.append(textField.text!)
+            
+            // Reload tableView after adding item
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+}
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+#if DEBUG
+
+
+struct Preview: PreviewProvider {
+    static var previews: some View {
+        // Assuming your storyboard file name is "Main"
+        UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier:"TDLVController").toPreview()
+    }
+}
+#endif
---
+++ Todoey/UIViewController + Preview.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewController + Preview.swift
+//  Todoey
+//
+//  Created by I9orila T on 16.08.2022.
+//
+
+import UIKit
+import SwiftUI
+
+#if DEBUG
+
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        // this variable is used for injecting the current view controller
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+
+    func toPreview() -> some View {
+        // inject self (the current view controller) for the preview
+        Preview(viewController: self)
+    }
+}
+#endif
