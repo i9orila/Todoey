b212abd75d7450f925c47f920f712b7a6d7919b7
I9orila <i9orila@aol.com>
2022-09-01 15:53:06 +0300

    add model save items in plist

--- Todoey/Controller/ToDoListViewController.swift
+++ Todoey/Controller/ToDoListViewController.swift
@@ -11,17 +11,27 @@ class ToDoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //print(dataFilePath)
+        
+    
         let newItem = Item()
         newItem.title = "Find Mike"
         itemArray.append(newItem)
         
+        let newItem1 = Item()
+        newItem1.title = "Find Marty"
+        itemArray.append(newItem1)
+        
     }
     
     
-    //MARK - Tableview Datasorse Methods
+    //MARK: - Tableview Datasorse Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -34,29 +44,31 @@ class ToDoListViewController: UITableViewController {
         
         cell.textLabel?.text = item.title
         
+    
+        // Ternary operator ==>
+        // value = condition ? valueTrue : valueFalse
         
-        
-        //MARK - 
-        
-        item.done ? (cell.accessoryType = .checkmark) : (cell.accessoryType = .none)
-        //cell.accessoryType = item.done ? .checkmark : .none
+        //item.done ? (cell.accessoryType = .checkmark) : (cell.accessoryType = .none)
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
     
-    //MARK - Tableview Delegate Methods
+    //MARK: - Tableview Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        saveItems()
+        
         tableView.deselectRow(at: indexPath, animated: true)
         
-        tableView.reloadData()
+
         
     }
     
-    //MARK - Add new items
+    //MARK: - Add new items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -69,8 +81,7 @@ class ToDoListViewController: UITableViewController {
             newItem.title = textField.text!
             self.itemArray.append(newItem)
             
-            // Reload tableView after adding item
-            self.tableView.reloadData()
+            self.saveItems()
         }
         
         alert.addTextField { (alertTextField) in
@@ -79,5 +90,21 @@ class ToDoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true)
+    }
+    
+//MARK: - Model Manipulation Method
+     
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        // Reload tableView after adding item
+        tableView.reloadData()
     }
 }
