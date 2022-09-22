495b3987964089e27519564a11b626f96d61415e
I9orila <i9orila@aol.com>
2022-09-22 17:28:15 +0300

    add category   view

--- Todoey/Controller/CategoryViewController.swift
+++ Todoey/Controller/CategoryViewController.swift
@@ -10,29 +10,96 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
     
+    var category = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext  //crud data
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        loadCategory()
     }
     
     //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return category.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCategoryCell", for: indexPath)
+        
+        let category = category[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        
+        
+        // Ternary operator ==>
+        // value = condition ? valueTrue : valueFalse
+        
+        //item.done ? (cell.accessoryType = .checkmark) : (cell.accessoryType = .none)
+        //cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    
     //MARK: - Data Manipulation Methods.
     //Save and load data (CRUD)
+    func saveCategory() {
+        do{
+            try context.save()
+        } catch {
+            print("Error saving context, \(error)")
+        }
+        
+        // Reload tableView after adding item
+        self.tableView.reloadData()
+    }
+    
+    func loadCategory(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            category = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context, \(error)")
+        }
+        tableView.reloadData()
+    }
     
     //MARK: - Add New Categories
     
-        @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title:"Add new category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
             
-            }
-
+            
+            self.category.append(newCategory)
+            
+            self.saveCategory()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
     
     
     //MARK: - TableView Delegate Methods
     
     
-
+    
     
     
 }
