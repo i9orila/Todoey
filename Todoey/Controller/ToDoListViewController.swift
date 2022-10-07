0ab93d3b23db1857b9f1a28af4b159ba6769f019
I9orila <i9orila@aol.com>
2022-10-07 14:33:34 +0300

    bug fix search bar

--- Todoey/Controller/ToDoListViewController.swift
+++ Todoey/Controller/ToDoListViewController.swift
@@ -138,15 +138,16 @@ extension ToDoListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request: NSFetchRequest<Item> = Item.fetchRequest()
         
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
     }
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0 {
             loadItems()
+            
             DispatchQueue.main.async {
                 searchBar.resignFirstResponder()
             }
